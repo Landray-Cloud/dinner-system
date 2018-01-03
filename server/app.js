bd98d6d811f361beb2e87d46a451897075b087f3
Leo <leochan2017@gmail.com>
2018-01-03 16:42:31 +0800

    1，提取路径前缀；2，insertData增加orderTime参数

--- server/app.js
+++ server/app.js
@@ -1,34 +1,36 @@
 const utils = require('./utils')
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const path = '/node/dinner/'
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('X-Powered-By', ' 3.2.1')
+    res.header('Content-Type', 'application/json;charset=utf-8')
+    next()
+})
 
-app.get('/node/dinner/getData', (req, res) => {
+app.get(path + 'getData', (req, res) => {
     utils.getData(dbRes => {
         res.send(dbRes)
     })
 })
 
-app.get('/node/dinner/insertData', (req, res) => {
+app.get(path + 'insertData', (req, res) => {
     let query = req.query
 
     utils.insertData({
         name: query.name,
         isOrder: query.isOrder,
+        dateTime: query.dateTime
     }, dbRes => {
         res.send(dbRes)
     })
 })
 
-app.get('/node/dinner/cleanData', (req, res) => {
+app.get(path +'cleanData', (req, res) => {
     let query = req.query
 
     utils.cleanData(dbRes => {
@@ -37,5 +39,5 @@ app.get('/node/dinner/cleanData', (req, res) => {
 })
 
 app.listen(3001, _ => {
-    console.log('listening on port 3001!');
-});
+    console.log('listening on port 3001!')
+})

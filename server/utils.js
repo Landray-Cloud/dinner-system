4014a4d05f4106dbb7fa4d87450bedb56a5cac56
caoye <724982540@qq.com>
2018-01-04 13:58:44 +0800

    服务器

--- server/utils.js
+++ server/utils.js
@@ -5,47 +5,48 @@ const path = '../'
 const dbPath = path + 'db/data.json'
 
 function getData(cb) {
-    fs.readFile(dbPath, 'utf-8', (err, data) => {
-        if (err) return console.log(err)
+  fs.readFile(dbPath, 'utf-8', (err, data) => {
+    if (err) return console.log(err)
 
-        if (!data) {
-            data = []
-        } else {
-            data = JSON.parse(data)
-        }
+    if (!data) {
+      data = []
+    } else {
+      data = JSON.parse(data)
+    }
 
-        cb && cb({ data, errmsg: 'ok', errcode: 0 })
-    })
+    cb && cb({ data, errmsg: 'ok', errcode: 0 })
+  })
 }
 
 function insertData(obj, cb) {
-    getData(res => {
-        if (!res) return cb && cb({ errcode: -1 })
+  getData(res => {
+    if (!res) return cb && cb({ errcode: -1 })
 
-        let data = res.data
+    let data = res.data
 
-        if (!data) return cb && cb({ errcode: -2 })
+    if (!data) return cb && cb({ errcode: -2 })
 
-        data.push(obj)
+    data.push(obj)
 
-        data = JSON.stringify(data)
+    data = JSON.stringify(data)
 
-        fs.writeFile(dbPath, data, err => {
-            if (err) return console.log(err)
-            cb && cb({ errmsg: 'ok', errcode: 0 })
-        });
-    })
+    fs.writeFile(dbPath, data, err => {
+      if (err) return console.log(err)
+      cb && cb({ errmsg: 'ok', errcode: 0 })
+    });
+  })
 }
 
 function cleanData(cb) {
-    fs.writeFile(dbPath, [], err => {
-        if (err) return console.log(err)
-        cb && cb({ errmsg: 'ok', errcode: 0 })
-    });
+  fs.writeFile(dbPath, [], err => {
+    if (err) return console.log(err)
+    cb && cb({ errmsg: 'ok', errcode: 0 })
+  });
 }
 
 module.exports = {
-    getData,
-    insertData,
-    cleanData
+  getData,
+  insertData,
+  cleanData
 }
+

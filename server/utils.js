8ca3ed19751ddd894938079369f39ef6d89c0a1e
Leo <leochan2017@gmail.com>
2018-01-10 16:15:35 +0800

    使用async/await 和 Promise 优雅 代码

--- server/utils.js
+++ server/utils.js
@@ -1,52 +1,91 @@
 'use strict'
 
-const fs = require('fs')
-const path = '../'
-const dbPath = path + 'db/data.json'
+const FS = require('fs')
+const PATH = '../'
+const DB_PATH = PATH + 'db/data.json'
+const SUCC_CB = { errmsg: 'ok', errcode: 0 }
+const ERROR_CB = { errmsg: 'error', errcode: -1 }
 
-function getData(cb) {
-  fs.readFile(dbPath, 'utf-8', (err, data) => {
-    if (err) return console.log(err)
+// 获取数据列表
+async function getList() {
+  return new Promise((resolve, reject) => {
+    FS.readFile(DB_PATH, 'utf-8', (err, content) => {
+      if (err) return reject(ERROR_CB)
 
-    if (!data) {
-      data = []
-    } else {
-      data = JSON.parse(data)
-    }
+      let data = content ? JSON.parse(content) : []
 
-    cb && cb({ data, errmsg: 'ok', errcode: 0 })
+      let cb = SUCC_CB
+      cb.data = data
+
+      resolve(cb)
+    })
   })
 }
 
-function insertData(obj, cb) {
-  getData(res => {
-    if (!res) return cb && cb({ errcode: -1 })
+// 清空列表
+async function cleanList() {
+  return new Promise((resolve, reject) => {
+    FS.writeFile(DB_PATH, [], err => {
+      if (err) return reject(ERROR_CB)
+      resolve(SUCC_CB)
+    })
+  })
+}
 
-    let data = res.data
+// 插入一条数据
+async function updateData(options) {
+  let dbRes = await getList()
 
-    if (!data) return cb && cb({ errcode: -2 })
+  return new Promise((resolve, reject) => {
+    if (!dbRes) return reject(ERROR_CB)
 
-    data.push(obj)
+    let data = dbRes.data
+
+    if (!data) return reject(ERROR_CB)
+
+    data.push(options)
 
     data = JSON.stringify(data)
 
-    fs.writeFile(dbPath, data, err => {
-      if (err) return console.log(err)
-      cb && cb({ errmsg: 'ok', errcode: 0 })
-    });
+    FS.writeFile(DB_PATH, data, err => {
+      if (err) return reject(ERROR_CB)
+      resolve(SUCC_CB)
+    })
   })
 }
 
-function cleanData(cb) {
-  fs.writeFile(dbPath, [], err => {
-    if (err) return console.log(err)
-    cb && cb({ errmsg: 'ok', errcode: 0 })
-  });
+
+// 用户当前是否点餐
+async function isOrder(name) {
+  let dbRes = await getList()
+
+  return new Promise((resolve, reject) => {
+    if (!dbRes) return reject(ERROR_CB)
+
+    let data = dbRes.data
+
+    if (!data) return reject(ERROR_CB)
+
+    let cb = SUCC_CB
+    cb.data = { isOrder: false }
+
+    if (!data.length) return reject(cb)
+
+    for (let item of data) {
+      if (item.name === name) {
+        cb.data.isOrder = true
+        break
+      }
+    }
+
+    resolve(cb)
+  })
 }
+
 
 module.exports = {
-  getData,
-  insertData,
-  cleanData
+  getList,
+  updateData,
+  cleanList,
+  isOrder
 }
-

0c9da8b1510099a523f7c9e4d2975088010c9d86
Leo <leochan2017@gmail.com>
2018-01-18 16:21:51 +0800

    文案精简

--- html/src/pages/UserFail.vue
+++ html/src/pages/UserFail.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home" class="mainBg">
     <div class="mainbox succ">
-      <h3 class="failtitle">当前非点餐时间段，如有疑问，请联系新梅！</h3>
+      <h3 class="failtitle">当前非点餐提交时间段！</h3>
     </div>
   </div>
 </template>

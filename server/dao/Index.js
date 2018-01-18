1057f25edb71c6863098ae3f3d34d4035638df75
Leo <leochan2017@gmail.com>
2018-01-18 17:06:13 +0800

    对addOrder和 updateDataById接口增加字段remarks

--- server/dao/Index.js
+++ server/dao/Index.js
@@ -4,7 +4,7 @@ let Index = {
     setSubmitUpdate: 'UPDATE submit_order SET status=? WHERE date=?',
     queryAction: 'SELECT * FROM list WHERE name=? AND orderDate=?',
     queryOrderStatus: 'SELECT * FROM list WHERE name=? AND orderDate=?',
-    insertOrder: 'INSERT INTO list(name, orderStatus, orderDate, orderTime) VALUES(?,?,?,?)'
+    insertOrder: 'INSERT INTO list(name, orderStatus, orderDate, orderTime, remarks) VALUES(?,?,?,?,?)'
 }
  
 module.exports = Index
--- server/dao/Manager.js
+++ server/dao/Manager.js
@@ -3,7 +3,7 @@ let Manager = {
     queryListByName: 'SELECT * FROM list WHERE name=?',
     queryListByDate: 'SELECT * FROM list WHERE orderDate=?',
     queryListByNameAndDate: 'SELECT * FROM list WHERE name=? AND orderDate=?',
-    updateById: 'UPDATE list SET orderStatus=?, orderDate=?, orderTime=?, name=? WHERE id=?',
+    updateById: 'UPDATE list SET orderStatus=?, orderDate=?, orderTime=?, name=?, remarks=? WHERE id=?',
     deleteById: 'DELETE from list where id=?',
     login: 'SELECT * FROM admin WHERE user=? AND pass=?',
     setSubmitUpdate: 'UPDATE submit_order SET status=? WHERE date=?'

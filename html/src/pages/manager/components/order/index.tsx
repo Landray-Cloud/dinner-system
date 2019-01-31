9b9881ec89f4bbd1373e1684636525387caf3bc9
liuchang <liu930505@qq.com>
2019-01-31 22:40:17 +0800

    添加数据（2）

--- html/src/pages/manager/components/order/index.tsx
+++ html/src/pages/manager/components/order/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Input, Button, Select, notification } from 'antd'
+import { Form, Input, Button, Select } from 'antd'
 import './index.scss'
 
 const Option = Select.Option
@@ -8,10 +8,34 @@ const FormItem = Form.Item
 interface IProps {
   history: any
 }
+interface IState {
+  name: string,
+  department: string,
+  orderStatus: string,
+  remarks: string,
+  form: {
+    [k: string]: string | number
+  }
+}
 
-class AddOrder extends Component<IProps> {
+class AddOrder extends Component<IProps, IState> {
   constructor(props) {
     super(props)
+    this.state = {
+      name: '',
+      department: '',
+      orderStatus: '',
+      remarks: '',
+      form: {
+        orderStatus: 0, // 提交用
+        remarks: ''
+      }
+    }
+  }
+  handleNameChange = (e) => {
+    const remarks = e.target.value
+    const form = Object.assign({}, this.state.form, { remarks })
+    this.setState({ form })
   }
   render() {
     const formItemLayout = {
@@ -22,7 +46,17 @@ class AddOrder extends Component<IProps> {
       <div className="form-warp">
         <Form className="order-form" layout="horizontal">
           <FormItem label="姓名" {...formItemLayout}>
-            <Input placeholder="请输入姓名" allowClear={true} />
+            <Input placeholder="请输入姓名" allowClear={true} onChange={this.handleNameChange}/>
+          </FormItem>
+          <FormItem label="部门" {...formItemLayout}>
+            <Select placeholder="请选择">
+              <Option value={0}>用户体验部</Option>
+              <Option value={1}>KM产品部</Option>
+              <Option value={2}>蓝钉产品部</Option>
+              <Option value={3}>平台支持部</Option>
+              <Option value={4}>EKP产品部</Option>
+              <Option value={5}>AIP部门</Option>
+            </Select>
           </FormItem>
           <FormItem label="订餐" {...formItemLayout}>
             <Select placeholder="请选择">

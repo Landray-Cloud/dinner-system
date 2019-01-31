460e97e8bc00f7b850a334e7d037ae1df3e81778
Leo <leochan2017@gmail.com>
2019-01-31 17:57:40 +0800

    开发配置

--- server/routes/Manager.js
+++ server/routes/Manager.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Manager = require('../modules/Manager')
 const auth = require('../components/auth')
 const AUTH_ERROR = { auth: 0 } // 统一登录失败
-const __IS_DEV__ = false
+const __IS_DEV__ = true
 
 // 后台登录
 router.post('/login', async (req, res) => {
